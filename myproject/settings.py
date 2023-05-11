6113e7494a7c575f71738544ee6873b06ecb62da
Alejandro López <83469483+Menosuno02@users.noreply.github.com>
2023-05-11 22:14:39 +0200

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -131,9 +131,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = ['/static/']
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
