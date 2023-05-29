8cb3c5de0c43cf342a38ce7c8b06a4da48d0667f
Alejandro López <mainusone1@gmail.com>
2023-05-29 14:51:45 +0200

    Update consumers

--- chat/consumers.py
+++ chat/consumers.py
@@ -55,7 +55,7 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
             }
         )
 
-    """ Mensajes del chat """
+    """ Mensaje del chat """
     async def chatroom_message(self, event):
         message = event['message']
         username = event['username']
